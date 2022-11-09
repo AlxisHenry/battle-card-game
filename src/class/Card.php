bbc84feba887328aa8b65e9c7e48ffbef52eba8d
AlxisHenry <alexis.henry150357@gmail.com>
2022-11-09 09:07:49 +0100

    Fix: type syntax

--- src/class/Card.php
+++ src/class/Card.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 class Card {
 
 	/**
+	 * Cards colors
+	 * 
 	 * @var array<string> SUITS
 	 */
 	public const SUITS = [
@@ -15,6 +17,8 @@ class Card {
 	];
 	
 	/**
+	 * Cards values
+	 * 
 	 * @var array<string> VALUES
 	 */
 	public const VALUES = [
@@ -31,34 +35,57 @@ class Card {
 	) {}
 
 	/**
+	 * Shuffle the cards
+	 * 
 	 * @param Game $game
 	 * @return void
 	 */
 	public static function shuffle(Game $game) : void
 	{	
+		/** 
+		 * @var array<Card> $cards 
+		 */
 		$cards = [];
+
+		/**
+		 * Generate all the cards 
+		 */
 		foreach (self::SUITS as $suit => $label) {
 			foreach (self::VALUES as $value => $label) {
 				$cards[] = new Card($suit, $value);
 			}
 		}
+		
 		shuffle($cards);
 		$game->setCards($cards);
 	}
 
 	/**
+	 * Distribute the cards to the players of the game
+	 * 
 	 * @param Game $game
 	 * @return void
 	 */
 	public static function distribute(Game $game): void
 	{
+		/** 
+		 * @var array<Player> $players Players of the current game 
+		 */
 		$players = $game->getPlayers();
+
+		/** 
+		 * @var array<Card> $cards Cards of the current game 
+		 */
 		$cards = $game->getCards();
+
 		foreach ($players as $i => $player) {
 			/** @var int $offset */
 			$offset = $i * (count($cards) / count($players));
-			/** @var int $length */
+			/** @var int $length Number of cards for each player */
 			$length = count($cards) / count($players);
+			/**
+			 * Distribute the cards to the current players
+			 */
 			$player->setCards(array_slice($cards, $offset, $length));
 		}
 	}
